5a72eb5bfb23df3a9d3a42f5d5cdfeb7495bbbb8
Dmitry Korochin <dmitrykorochin@MacBook-Pro-Dmitry.local>
2025-12-17 18:11:55 +0300

    Default project

---
+++ Storyteller/Storyteller/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Storyteller
+//
+//  Created by Dmitry Korochin on 17.12.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Storyteller/StorytellerTests/StorytellerTests.swift
@@ -0,0 +1,17 @@
+//
+//  StorytellerTests.swift
+//  StorytellerTests
+//
+//  Created by Dmitry Korochin on 17.12.2025.
+//
+
+import Testing
+@testable import Storyteller
+
+struct StorytellerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
